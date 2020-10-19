d31396a35e1b17ee2368fa1acd3592194004dd31
Aminkta <amin.kouta@hotmail.com>
2020-10-19 07:35:15 -0400

    changed app text

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          <code>src/App.js</code>  hakuna matata.
+          I was changed to hakuna matata.
         </p>
         <a
           className="App-link"
